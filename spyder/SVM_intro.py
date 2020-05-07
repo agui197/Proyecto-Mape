48001c1e70c31aa35e21b933982774898f37320c
RUIZ CRUZ <RIEMANNRUIZ@iteso.mx>
2020-05-07 14:02:01 -0500

    Update SVM_intro.py
    Se termino el ejemplo de clasificación en python

--- spyder/SVM_intro.py
+++ spyder/SVM_intro.py
@@ -35,21 +35,37 @@ H = np.multiply(K,Ym)
 onev = -np.ones((y.shape[0],1))
 Aeq = np.float64(np.reshape(y,(1,y.shape[0])))
 beq = 0.0
+c=0.1
+G = np.float64(np.concatenate((np.identity(y.shape[0]),-np.identity(y.shape[0]))))
+#G = np.float64(np.identity(y.shape[0]))
+h=np.float64(np.concatenate((c*np.ones((y.shape[0],1)),np.zeros((y.shape[0],1)))))
 
-#%%
+#%% Solucion al problema de optimizacion cuadratica
 from cvxopt import matrix, solvers
 P = matrix(H)
 q = matrix(onev)
 A = matrix(Aeq)
 b = matrix(beq)
-sol=solvers.qp(P=P, q=q, A=A, b=b)
+G = matrix(G)
+h = matrix(h)
+sol=solvers.qp(P=P, q=q,G=G,h=h, A=A, b=b)
 
-#%%
-#from cvxopt import matrix, solvers
-#Q = 2*matrix([ [2, .5], [.5, 1] ])
-#p = matrix([1.0, 1.0])
-#G = matrix([[-1.0,0.0],[0.0,-1.0]])
-#h = matrix([0.0,0.0])
-#A = matrix([1.0, 1.0], (1,2))
-#b = matrix(1.0)
-#sol=solvers.qp(P=Q, q=p, A=A, b=b)
+alphas = np.array(sol['x'])
+
+#%% Support vector values
+indx = alphas > 1e-10
+alpha_sv = alphas[indx]
+x_sv = Xm[indx[:,0],:]
+y_sv = y[indx[:,0]]
+
+#%% Calculo de las w
+w = np.sum(np.c_[alpha_sv*y_sv,alpha_sv*y_sv]*x_sv,axis=0)
+b = np.mean(1/y_sv-np.dot(x_sv,w))
+
+#%% Visualizar los datos
+plt.scatter(x1m,x2m,c=y)
+plt.plot(x1,(-w[0]*x1-b)/w[1])
+plt.xlim(-10,10)
+plt.ylim(-10,10)
+plt.show()
+
