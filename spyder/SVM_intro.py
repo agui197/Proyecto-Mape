b31690ce38ea8727a2bbaa46f70feab76615341c
Alfonso Aguilar <tripleaguilar@hotmail.com>
2020-05-10 19:57:11 -0500

    Format scripts

--- spyder/SVM_intro.py
+++ spyder/SVM_intro.py
@@ -6,66 +6,70 @@ Created on Wed May  6 12:13:54 2020
 """
 
 #Importar librerias
-import numpy as np
-import matplotlib.pyplot as plt
+from numpy import linspace, meshgrid, matrix, ravel, c_, multiply, \
+    ones, float64, reshape, concatenate, identity, zeros, array, sum, mean, dot
+from matplotlib.pyplot import scatter, show, xlim, ylim, plot
+from sklearn.metrics.pairwise import linear_kernel
+from cvxopt import matrix as mat
+from cvxopt import solvers
 
 #%% Generacion de los datos
 n = 20
-x1 = np.linspace(-10,10,n)
-x2 = np.linspace(-10,10,n)
-X1,X2 = np.meshgrid(x1,x2)
-Y = 2*((np.matrix(X1)**3-3*X1)>X2)-1
+x1 = linspace(-10, 10, n)
+x2 = linspace(-10, 10, n)
+X1, X2 = meshgrid(x1, x2)
+Y = 2 * ((matrix(X1)**3 - 3 * X1) > X2) - 1
 
-x1m = np.ravel(X1.T)
-x2m = np.ravel(X2.T)
-Xm = np.c_[x1m,x2m]
-y = np.ravel(Y.T)
-Ym = np.matrix(y).T*np.matrix(y)
+x1m = ravel(X1.T)
+x2m = ravel(X2.T)
+Xm = c_[x1m, x2m]
+y = ravel(Y.T)
+Ym = matrix(y).T * matrix(y)
 
 #%% Visualizar los datos
-plt.scatter(x1m,x2m,c=y)
-plt.show()
+scatter(x1m, x2m, c=y)
+show()
 
 #%% Kernel matrix
-from sklearn.metrics.pairwise import linear_kernel
-K = linear_kernel(Xm,Xm)
+
+K = linear_kernel(Xm, Xm)
 
 #%% Optimization Variables
-H = np.multiply(K,Ym)
-onev = -np.ones((y.shape[0],1))
-Aeq = np.float64(np.reshape(y,(1,y.shape[0])))
+H = multiply(K, Ym)
+onev = -ones((y.shape[0], 1))
+Aeq = float64(reshape(y, (1, y.shape[0])))
 beq = 0.0
-c=0.1
-G = np.float64(np.concatenate((np.identity(y.shape[0]),-np.identity(y.shape[0]))))
-#G = np.float64(np.identity(y.shape[0]))
-h=np.float64(np.concatenate((c*np.ones((y.shape[0],1)),np.zeros((y.shape[0],1)))))
+c = 0.1
+G = float64(concatenate((identity(y.shape[0]), -identity(y.shape[0]))))
+# G = np.float64(np.identity(y.shape[0]))
+h = float64(concatenate((c * ones((y.shape[0], 1)), zeros((y.shape[0], 1)))))
 
 #%% Solucion al problema de optimizacion cuadratica
-from cvxopt import matrix, solvers
-P = matrix(H)
-q = matrix(onev)
-A = matrix(Aeq)
-b = matrix(beq)
-G = matrix(G)
-h = matrix(h)
-sol=solvers.qp(P=P, q=q,G=G,h=h, A=A, b=b)
 
-alphas = np.array(sol['x'])
+P = mat(H)
+q = mat(onev)
+A = mat(Aeq)
+b = mat(beq)
+G = mat(G)
+h = mat(h)
+sol=solvers.qp(P=P, q=q, G=G, h=h, A=A, b=b)
+
+alphas = array(sol['x'])
 
 #%% Support vector values
 indx = alphas > 1e-10
 alpha_sv = alphas[indx]
-x_sv = Xm[indx[:,0],:]
-y_sv = y[indx[:,0]]
+x_sv = Xm[indx[:, 0], :]
+y_sv = y[indx[:, 0]]
 
 #%% Calculo de las w
-w = np.sum(np.c_[alpha_sv*y_sv,alpha_sv*y_sv]*x_sv,axis=0)
-b = np.mean(1/y_sv-np.dot(x_sv,w))
+w = sum(c_[alpha_sv * y_sv, alpha_sv * y_sv] * x_sv, axis=0)
+b = mean(1 / y_sv - dot(x_sv, w))
 
 #%% Visualizar los datos
-plt.scatter(x1m,x2m,c=y)
-plt.plot(x1,(-w[0]*x1-b)/w[1])
-plt.xlim(-10,10)
-plt.ylim(-10,10)
-plt.show()
+scatter(x1m, x2m, c=y)
+plot(x1,(-w[0] * x1 - b) / w[1])
+xlim(-10, 10)
+ylim(-10, 10)
+show()
 
